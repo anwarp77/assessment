56618fcf610f11bbcbef620eb001ab91946681c0
anwarp77 <129684495+anwarp77@users.noreply.github.com>
2023-06-22 11:16:08 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import re
-from Bio.Seq import Seq
-string = "AATAGAGCAGTGTCGCGCACTCTGCGTCACGATGTGTGGTCTATGACAGTTCTTTTTTTGTGTGGTTACGCGGACCACCGAGATTTCACCCGCCGTGGATATAC"
-rev_com = Seq(string).reverse_complement()
-f_primer = Seq("AGTGTCGCGCACT").reverse_complement()
-r_primer = Seq("ACCGAGATTTCAC").reverse_complement()
-result = re.search(f'{f_primer}.*{r_primer}', rev_com)
-print(result)
